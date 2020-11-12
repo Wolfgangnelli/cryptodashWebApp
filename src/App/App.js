96a460e4f5b432f1927a6d41b27c73b818fb3152
AndreaS <solfa_90@hotmail.it>
2020-11-12 11:50:57 +0100

    Add AppBar and grid

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Welcome from "../Components/Welcome";
 import AppLayout from "../Components/AppLayout";
+import AppBar from "../Components/AppBar";
 
 function App() {
   return (
     <AppLayout>
+      <AppBar />
       <Welcome name={"Cryptodash"} />
     </AppLayout>
   );
